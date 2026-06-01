3c71379258d4a8e78e6bd6206cee88c0f0353d1a
luisyangzw <luisyangzw6@gmail.com>
2026-06-01 10:27:57 -0700

    Update pickup prefab and victory checker

--- Assets/Scenes/EnemyVictoryChecker.cs
+++ Assets/Scenes/EnemyVictoryChecker.cs
@@ -35,7 +35,7 @@ public class EnemyVictoryChecker : MonoBehaviour
         CheckForVictory();
     }
 
-    private void HandleEnemyDied()
+    private void HandleEnemyDied(EnemyHealth _)
     {
         CheckForVictory();
     }
